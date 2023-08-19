f51f98cb3abbb17e62ff9c1ef9b16c6fff17a4c0
AJ-droi <ajiriosiobe@gmail.com>
2023-08-19 13:20:44 +0100

    portfolio v2023

---
+++ constants/data.js
@@ -0,0 +1,126 @@
+const userData = {
+  githubUsername: "AJ-droi",
+  name: "Ajiri Enoch Osiobe",
+  designation: "MERN-Stack Developer",
+  avatarUrl: "/profile.jpg",
+  email: "ajiriosiobe@gmail.com",
+  phone: "+234 8104228894",
+  address: "https://github.com/AJ-droi",
+  projects: [
+    {
+      title: "The Gathering",
+      link: "https://thegathering.com.ng/",
+      imgUrl: "/thegathering.png",
+    },
+    {
+      title: "Black Britist Women Exist",
+      link: "https://blackbritishwomenexist.com/",
+      imgUrl: "/bbweapp.png",
+    },
+    {
+      title: "BlackWomenExist Business App",
+      link: "https://www.blackwomenexistbusinessapp.com/",
+      imgUrl: "/bbwe.png",
+    },
+    {
+      title: "Dat Visualization using D3js, React and Azure",
+      link: "https://reactt-azure.azurewebsites.net/",
+      imgUrl: "/d3js.png",
+    },
+    {
+      title: "Smooze Music Application",
+      link: "https://smoozepro.netlify.app/",
+      imgUrl: "/smoozeApp.png",
+    },
+    {
+      title: "Sickle Cell Awareness Health Foundation Website",
+      link: "https://scahf.netlify.com",
+      imgUrl: "/scahf.png",
+    },
+    {
+      title: "PeersApp Landing Page",
+      link: "https://peers-app.netlify.app",
+      imgUrl: "/peersapp.png",
+    },
+    {
+      title: "Portfolio",
+      link: "https://ajiriosiobe.netlify.app/",
+      imgUrl: "/ajiri.png",
+    },
+    {
+      title: "Ahuizi Investment Ltd",
+      link: "https://ahuiziinvest.com",
+      imgUrl: "/Agriculture.png",
+    },
+    {
+      title: "Veeys UI/UX Portfolio Website",
+      link: "https://veeys-portfolio.netlify.app/",
+      imgUrl: "/veeysPortfolio.png",
+    },
+    {
+      title: "ArticTravels",
+      link: "https://articc-travels.netlify.app/",
+      imgUrl: "/articTravels.png",
+    },
+    {
+      title: "ColorFlipperJs",
+      link: "https://colorfllipper.netlify.app/",
+      imgUrl: "/colorFlipper.png",
+    },
+   
+  ],
+  about: {
+    title:
+      "I am a fullsatck software engineer that loves building tech products and providing innovative solutions to enterprises.",
+    description: [
+      `I 'm a Customer-focused software professional with superior communication skills and expertise in Consumption of RestFul API’s and Front-end development frameworks including ReactJs. I Understands and meet both client and employer needs and develops highly functional, robust and easy-to-use applications. I have Excelled as a hands-on developer and oversees work of agile software teams.
+      `,
+      
+    ],
+    currentProject: "Music Player Application",
+    // currentProjectUrl: "https://tailwindmasterkit.com",
+  },
+  experience: [
+    {
+      title: "Full Stack Engineer",
+      company: "Decagon",
+      year: "2022-",
+      companyLink: "https://mroads.com",
+      desc: "Built reliable software and online applications with over 92% client approval rate using Node.js and React.js that satisfy current market standards, Used Github for website application, Continuous Integration and Deployment for optimal software performances. Software API documentation using Swagger. Before merging code to staging, numerous tests were written to find defects and other technical concerns, reducing code smells and bugs by over 90%.",
+    },
+    {
+      title: "Lead Developer (Contract)",
+      company: "PeersApp StartUp, Remote",
+      year: "2022",
+      companyLink: "https://tailwindmasterkit.com",
+      desc: "Aided in facilitating the smooth transfer of It from the Boot Camp Phase to the Company Registration Phase. Built a Restful API for its mobile application using Node and Typescript, which assisted the business in securing funding and enhanced its financial structure by 65%. Updated the code structure and decreased its complexity, increasing coding productivity by 30% and making it simpler for other developers to adapt to the code. Assisted the team's seven new junior front-end engineers in developing their knowledge of Javascript, typescript, ReactJS, NodeJS, and NextJS."
+    },
+    {
+      title: "Front End Instructor (Contract)",
+      company: "EarlyBrite Ltd, Remote",
+      year: "2022",
+      companyLink: "https://algochurn.com",
+      desc: "Trained Young Minds between the age of 9-17 on scripting and frontend languages like HTML, CSS, Bootstrap andReact. Recorded 75% significant output In technical abilities of trainees. Built Amazing Projects like Landing Pages, javascript Counter, Javascript Calculators and Github Page hosting."
+    },
+    {
+      title: "Front End Developer",
+      company: "Stem Professionals Ltd, Port Harcourt, Nigeria",
+      year: "2020",
+      companyLink: "https://upes.co.in",
+      desc: "Physically and remotely instructed more than 300 pupils in frontend languages like HTML, CSS, Bootstrap andReact across several schools in Port-Harcourt. Contributed and worked as a team member in Port Harcourt Robotic Contest. Promoted from Intern to an Employe and to a Front End Team Lead.Education",
+    },
+  ],
+  frontEndUrl:
+    "https://docs.google.com/document/d/1IrbOmyM0XwHGkC9evsGvQuPj2Tm1q0lz76qgsMpJfGo/edit?usp=share_link",
+  backEndUrl: "https://docs.google.com/document/d/18VGtOvf15etTf05QS7CHL49eTx4tO-HGCmbg47yEl_w/edit?usp=sharing",
+  
+  socialLinks: {
+    // instagram: "https://instagram.com/manint",
+    twitter: "https://twitter.com/osiobe_ajiri",
+    linkedin: "https://www.linkedin.com/in/ajiri-osiobe-801675184/",
+    github: "https://github.com/AJ-droi",
+    // facebook: "https://facebook.com/",
+  },
+};
+
+export default userData;
